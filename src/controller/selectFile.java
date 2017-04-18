ae3ffd7230422bce55cc2808ae35d21156e04b2d
Walter <adriel.walter@cgi.com>
2017-04-18 19:19:17 +0200

    bug: Wrong  update on file 1

--- src/controller/selectFile.java
+++ src/controller/selectFile.java
@@ -19,14 +19,15 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
 
 /**
  * Created by Adriel on 4/9/2017.
  */
 public class selectFile extends JFrame {
-    public String filepath;
-    public Document doc;
-
+    private String filepath;
+    private Document doc;
+    private StringBuilder filelist = new StringBuilder("nothing");
 
     public selectFile() {
         super("File Chooser Test Frame");
@@ -51,49 +52,51 @@ public class selectFile extends JFrame {
                 int option = chooser.showOpenDialog(selectFile.this);
                 if (option == JFileChooser.APPROVE_OPTION) {
                     File[] sf = chooser.getSelectedFiles();
-                    String filelist = "nothing";
-                    if (sf.length > 0) filelist = sf[0].getName();
+
+                    if (sf.length > 0) filelist = new StringBuilder(sf[0].getName());
                     for (int i = 1; i < sf.length; i++) {
-                        filelist += ", " + sf[i].getName();
+                        filelist.append(",").append(sf[i].getName());
                     }
                     statusbar.setText("You chose " + filelist);
 
-                    try {
-                        filepath = filelist;
-                        DocumentBuilderFactory docFactory = DocumentBuilderFactory.newInstance();
-                        DocumentBuilder docBuilder = docFactory.newDocumentBuilder();
-                        doc = docBuilder.parse(filepath);
+                    ArrayList<String> dataFile = openMultiple(filelist.toString());
+                    for (String aDataFile : dataFile) {
+                        System.out.println(aDataFile);
+                        try {
+                            filepath = aDataFile;
+                            DocumentBuilderFactory docFactory = DocumentBuilderFactory.newInstance();
+                            DocumentBuilder docBuilder = docFactory.newDocumentBuilder();
+                            doc = docBuilder.parse(aDataFile);
 
-                        // Get the root element
-                        Node company = doc.getFirstChild();
+                            Document docs = documentUpload(aDataFile);
 
-                        // Get the staff element by tag name directly
-                        Node staff = doc.getElementsByTagName("staff").item(0);
-                        // loop the staff child node
-                        NodeList list = staff.getChildNodes();
+                            // Get the staff element by tag name directly
+                            Node staff = doc.getElementsByTagName("staff").item(0);
+                            // loop the staff child node
+                            NodeList list = staff.getChildNodes();
 
-                        for (int i = 0; i < list.getLength(); i++) {
-                            Node node = list.item(i);
+                            for (int i = 0; i < list.getLength(); i++) {
+                                Node node = list.item(i);
 
-                            // get the salary element, and update the value
-                            if ("salary".equals(node.getNodeName())) {
-                                String xmlVal = node.getTextContent();
-                                //get the 18 number of the list
-                                int replaceChart = Integer.parseInt(xmlVal.substring(18, 22));
-                                int counter = replaceChart + 1;
-                                StringBuilder stringBuilder = new StringBuilder(xmlVal);
-                                stringBuilder.replace(18, 22, String.format("%04d", counter));
-                                String newMyName = stringBuilder.toString();
+                                // get the salary element, and update the value
+                                if ("salary".equals(node.getNodeName())) {
+                                    String xmlVal = node.getTextContent();
+                                    //get the 18 number of the list
+                                    int replaceChart = Integer.parseInt(xmlVal.substring(18, 22));
+                                    int counter = replaceChart + 1;
+                                    StringBuilder stringBuilder = new StringBuilder(xmlVal);
+                                    stringBuilder.replace(18, 22, String.format("%04d", counter));
+                                    String newMyName = stringBuilder.toString();
 
-                                System.out.println(newMyName);
-                                node.setTextContent(newMyName);
+                                    System.out.println(newMyName);
+                                    node.setTextContent(newMyName);
+                                }
                             }
-                        }
 
-                    } catch (ParserConfigurationException | IOException | SAXException pce) {
-                        pce.printStackTrace();
-                    }
-                } else {
+                        } catch (ParserConfigurationException | IOException | SAXException pce) {
+                            pce.printStackTrace();
+                        }
+                }} else {
                     statusbar.setText("You canceled.");
                 }
             }
@@ -102,26 +105,26 @@ public class selectFile extends JFrame {
         // Create a file chooser that opens up as a Save dialog
         runButton.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent ae) {
-
+                ArrayList<String> dataFile = openMultiple(filelist.toString());
+                for (String aDataFile : dataFile) {
                 try {
                     // write the content into xml file
                     TransformerFactory transformerFactory = TransformerFactory.newInstance();
                     Transformer transformer = transformerFactory.newTransformer();
                     DOMSource source = new DOMSource(doc);
-                    StreamResult result = new StreamResult(new File(filepath));
+                    StreamResult result = new StreamResult(new File(aDataFile));
                     if (result == null) {
                         System.out.println("no file select");
                     }
                     transformer.transform(source, result);
 
-                    System.out.println("Done");
-                    statusbar.setText("Done");
+                    System.out.println(aDataFile +" Was successfully Done");
+                    statusbar.setText(dataFile.size() +"Was successfully Done");
 
                 } catch (TransformerException e) {
                     e.printStackTrace();
                     statusbar.setText("Error");
-                }
-                statusbar.setText("Done");
+                }}
             }
         });
 
@@ -130,4 +133,21 @@ public class selectFile extends JFrame {
         c.add(statusbar);
     }
 
+    private ArrayList<String> openMultiple(String file) {
+        String[] fields = file.split(",");
+        ArrayList<String> myData = new ArrayList<>();
+        for (String field : fields) {
+            file = field;
+            myData.add(file);
+        }
+//        System.out.println(myData);
+        return myData;
+    }
+
+    private Document documentUpload(String file) throws ParserConfigurationException, IOException, SAXException {
+        DocumentBuilderFactory docFactory = DocumentBuilderFactory.newInstance();
+        DocumentBuilder docBuilder = docFactory.newDocumentBuilder();
+        doc = docBuilder.parse(file);
+        return doc;
+    }
 }
